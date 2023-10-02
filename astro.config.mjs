09a0c316226523ee8852b9161b7f3fbdb49993a1
badstagram <4477658-badstagram@users.noreply.gitlab.com>
2023-10-02 19:02:55 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'vessel-docs.badstagram.gay',
 	integrations: [
 		starlight({
 			title: 'Vessel',
